9049d8a99b47679f1dd4c01da7560d7b976cbe82
VolodymyrGavrysh <vladimir101175@gmail.com>
2020-03-05 15:55:21 +0200

    last changes to post for user pic

--- users/models.py
+++ users/models.py
@@ -15,14 +15,14 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 
-    def save(self, *args, **kwargs): # change the save method
-        super().save(*args, **kwargs)
-        # image
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # def save(self, *args, **kwargs): # change the save method
+    #     super().save(*args, **kwargs)
+    #     # image
+    #     img = Image.open(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
 
 
